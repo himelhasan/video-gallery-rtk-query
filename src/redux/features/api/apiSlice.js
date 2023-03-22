48c30cf6d3c56769565f0918d8cb2d0c027ecbd9
himelhasan <himelhasan497@gmail.com>
2023-03-22 23:13:30 +0600

    realted videos loading based on the title tags

--- src/redux/features/api/apiSlice.js
+++ src/redux/features/api/apiSlice.js
@@ -9,7 +9,18 @@ export const apiSlice = createApi({
     getVideos: builder.query({
       query: () => "/videos",
     }),
+    getVideo: builder.query({
+      query: (VideoId) => `/videos/${VideoId}`,
+    }),
+    getRelatedVideos: builder.query({
+      query: ({ id, title }) => {
+        const tags = title.split(" ");
+        const string = tags.map((tag) => `title_like=${tag}`).join("&");
+        const url = `/videos?${string}&_limit=4`;
+        return url;
+      },
+    }),
   }),
 });
 
-export const { useGetVideosQuery } = apiSlice;
+export const { useGetVideosQuery, useGetVideoQuery, useGetRelatedVideosQuery } = apiSlice;
